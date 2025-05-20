926aeaa41c53203f95e4812d6808562781697945
Juan Afanador <juanafanador07@gmail.com>
2025-05-20 10:27:11 -0500

    feat: Add logout on NuevoUsuario

--- src/pages/dashboard/datos-personales/NuevoUsuario.tsx
+++ src/pages/dashboard/datos-personales/NuevoUsuario.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Navigate, useNavigate } from "react-router";
 import { toast } from "sonner";
 
+import { Button } from "@/components/ui/button";
 import useAuth from "@/hooks/useAuth";
 import { DatosPersonalesFormOutput } from "@/schemas/datos-personales";
 import { create as createDatosPersonales } from "@/services/datos-personales";
@@ -10,11 +11,7 @@ import DatosPersonalesForm from "./components/DatosPersonalesForm";
 
 export default function NuevoUsuario() {
   const navigate = useNavigate();
-  const { refreshInfo } = useAuth();
-
-  useEffect(() => {
-    refreshInfo();
-  }, []);
+  const { refreshInfo, auth, info } = useAuth();
 
   async function onCreate(datosPersonales: DatosPersonalesFormOutput) {
     await createDatosPersonales(datosPersonales);
@@ -24,8 +21,21 @@ export default function NuevoUsuario() {
     navigate("/dashboard");
   }
 
+  if (info?.hasPersonalData) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
     <div>
+      <Button
+        className="absolute top-6 right-6 shadow"
+        size="icon"
+        variant="secondary"
+        onClick={() => auth.signOut()}
+      >
+        <Icon icon="material-symbols:logout-rounded" className="size-6" />
+        <span className="sr-only">Cerrar sesión</span>
+      </Button>
       <h1 className="mb-4 text-2xl font-bold">
         Necesitamos más información sobre tí
       </h1>
